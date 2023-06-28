56ea55a23df13eb551e4b108b4fca0ebf0bb7b98
Mohd Shadab <shadab_ibrahim@outlook.com>
2023-06-28 16:48:52 +0530

    submitting code-Wed Jun 28 2023 16:48:51 GMT+0530 (India Standard Time)

--- script.js
+++ script.js
@@ -1,3 +1,14 @@
 const url = "https://jsonplaceholder.typicode.com/posts/1";
 
 //your JS code here. If required.
+let btn = document.getElementById("btn");
+let result = document.getElementById("output");
+
+btn.addEventListener('click',fetchData)
+
+async function fetchData() {
+	let data = await fetch("https://jsonplaceholder.typicode.com/posts/1");
+	data = await data.json();
+    // console.log(data.title)
+	result.innerHTML = data.title;
+}
